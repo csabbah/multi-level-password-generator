7b64d7e2d5a9995b55117fab1908c94f1476b256
Csabbah <csabbah@ryerson.ca>
2022-03-06 12:09:52 -0500

    Password clears upon generating a new one and update message styling to match with state

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var passwordText = document.querySelector('#password');
 const filters = document.querySelector('.card-criteria');
 const cardFooter = document.querySelector('.card-footer');
 const updateMsg = document.getElementById('update-msg');
-var runOnce = true; // Declare this variable to determine when to reveal filter opttions
+var runOnce = true; // Declare this variable to determine when to reveal filter options
 // and when to execute the generate password function
 
 // --------------- Declare all necessary parameter for creating the password
@@ -20,7 +20,14 @@ var parameters = {
 };
 
 // --------------- This function displays error message if condition is not met
-function displayError() {
+function displayUpdate() {
+  // If password is active, message should be color in green
+  if (!password.value == '') {
+    updateMsg.style.color = 'green';
+  } else {
+    // Else it should be red since it will be about failing to meet requirement
+    updateMsg.style.color = 'red';
+  }
   // Upon initial execution, reveal the message
   updateMsg.style.display = 'flex';
   // Then after 2 seconds, hide it
@@ -33,15 +40,27 @@ function revealFilters() {
   // Set display to flex and extend height of footer
   filters.style.display = 'flex';
   cardFooter.style.height = '140px';
+  // Set to false so we can execute generatePassword()
   runOnce = false;
+  // Change button label to match with state
   generateBtn.innerHTML = 'Generate Password';
+  // Only clear the password if it exists and inform user that filters have been reset
+  if (!password.value == '') {
+    displayUpdate();
+    updateMsg.innerText = 'Filters reset';
+    password.value = '';
+  }
 }
 
 // --------------- After successful password generation, clear all input fields
 function clearFilters() {
   document.getElementById('numOfChar').value = '';
-  numOfChar = '';
+  document.querySelectorAll('.checkbox').forEach((item) => {
+    item.checked = false;
+  });
+  // By setting to true, we can now execute revealFilter()
   runOnce = true;
+  // Revert to initial card style
   filters.style.display = 'none';
   cardFooter.style.height = 'initial';
 }
@@ -79,7 +98,7 @@ function generatePassword() {
     tempReqs[3] == false &&
     numOfChar == ''
   ) {
-    displayError();
+    displayUpdate();
     updateMsg.innerText =
       'Please use at least one criteria and fill in character count.';
   } else if (
@@ -94,7 +113,7 @@ function generatePassword() {
       tempReqs[3] == false &&
       numOfChar < 8)
   ) {
-    displayError();
+    displayUpdate();
     updateMsg.innerText =
       'Please use at least one criteria and choose a valid character count between 8 and 128';
   } else if (
@@ -103,23 +122,21 @@ function generatePassword() {
     tempReqs[2] == false &&
     tempReqs[3] == false
   ) {
-    displayError();
+    displayUpdate();
     updateMsg.innerText = 'Please use at least one criteria.';
   } else if (numOfChar < 8 || numOfChar > 128) {
-    displayError();
+    displayUpdate();
     updateMsg.innerText =
       'Please type a valid character count between 8 and 128';
   } else {
     // If all conditions are met, then generate password
     var password = 'abcdefghi';
     // Temporarily create an array out of the password so we can shuffle it
-    const shuffledArray = password
-      .split('')
-      .sort((a, b) => 0.5 - Math.random());
-
-    passwordText.value = shuffledArray;
+    const passShuffled = password.split('').sort((a, b) => 0.5 - Math.random());
+    // Set passwordText to the new shuffled password
+    passwordText.value = passShuffled;
+    // Change button label to match with state
     generateBtn.innerHTML = 'Generator another';
-
     // Revert back to the original state
     clearFilters();
   }
@@ -132,6 +149,7 @@ generateBtn.addEventListener('click', () => {
     revealFilters();
   } else {
     // Then execute this function after
+    // Upon successful password generation, set runOnce to true
     generatePassword();
   }
 });
